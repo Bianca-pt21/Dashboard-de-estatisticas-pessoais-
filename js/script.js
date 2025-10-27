26d193050e1f5178318c0be1c669e15b0e8d242c
BIANCA DO COUTO SANTOS <pessoalc505@gmail.com>
2025-10-27 13:08:19 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
+const ctx = document.getElementById('myChart');
+
+new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: ['HTML', 'CSS', 'JS', 'Python', 'Git'],
+    datasets: [{
+      label: 'Nível de Habilidade (%)',
+      data: [90, 85, 75, 60, 80],
+      borderWidth: 1
+    }]
+  },
+  options: {
+    scales: { y: { beginAtZero: true } }
+  }
+});
 
